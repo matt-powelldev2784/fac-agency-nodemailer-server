1de4ce390354c9581d3440f1954421d8d09764ed
Matthew Powell <matt.powell2784@gmail.com>
2023-05-23 16:21:40 +0100

    add cors support

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { sendEmail } = require('./sendEmail')
 const app = express()
 app.use(express.json())
 
+app.use(cors())
+app.options('*', cors())
+
 const PORT = process.env.PORT || 5001
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
