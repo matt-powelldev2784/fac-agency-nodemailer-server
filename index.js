ce86d1c1b3f1076e994e4e2fd5f530c1753ef7c0
Matthew Powell <matt.powell2784@gmail.com>
2023-05-23 13:15:18 +0100

    chnage dev port to 5001

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.end()
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
