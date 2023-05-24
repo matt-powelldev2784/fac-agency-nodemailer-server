37fb42db00bfa237dd2c2e03801751c298c36307
Matthew Powell <matt.powell2784@gmail.com>
2023-05-24 13:13:15 +0100

    provide detailed api responses

--- index.js
+++ index.js
@@ -17,18 +17,36 @@ app.post('/send-email', async (req, res) => {
   const { email, name } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'Email is required' })
+    return res.status(400).send({
+      success: false,
+      status: 400,
+      error: 'Email is required',
+    })
   }
 
   if (!name) {
-    return res.status(400).json({ error: 'Name is required' })
+    return res.status(400).send({
+      success: false,
+      status: 400,
+      error: 'Name is required',
+    })
   }
 
   try {
     await sendEmail(req.body)
-    res.status(200).json({ message: `Email sent successfully to ${email}` })
+
+    return res.status(200).send({
+      success: true,
+      status: 200,
+      message: `Email sent successfully to ${email}`,
+    })
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: 'Failed to send email' })
+
+    return res.status(500).send({
+      success: false,
+      status: 500,
+      error: 'Failed to send email',
+    })
   }
 })
